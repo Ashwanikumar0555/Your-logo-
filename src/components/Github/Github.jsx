9fe14f93e918bc91d257b5ed581799588b5fa42b
Ashwani kumar <ashwanikumar05556@gmail.com>
2024-12-28 00:18:18 +0530

    add the file github

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,31 +1,31 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// function Github() {
-//     const [data, setData] = useState(null); 
+function Github() {
+    const [data, setData] = useState(null); 
 
-//     useEffect(() => {
-//         fetch('https://api.github.com/users/Ashwanikumar0555') 
-//             .then(response => response.json())
-//             .then(data => {
-//                 console.log(data);
-//                 setData(data);
-//             })
-//             .catch(error => console.error('Error fetching data:', error)); 
-//     }, []);
+    useEffect(() => {
+        fetch('https://api.github.com/users/Ashwanikumar0555') 
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setData(data);
+            })
+            .catch(error => console.error('Error fetching data:', error)); 
+    }, []);
 
-//     return (
-//         <div className="text-center text-white m-4 font-semibold bg-blue-200 p-4 text-3xl">
+    return (
+        <div className="text-center text-white m-4 font-semibold bg-blue-200 p-4 text-3xl">
            
-//             {data ? (
-//                 <>
-//                     <p>Github Followers: {data.followers}</p>
-//                     <img src={data.avatar_url} alt="GitHub Avatar" width={300} />
-//                 </>
-//             ) : (
-//                 <p>Loading...</p>
-//             )}
-//         </div>
-//     );
-// }
+            {data ? (
+                <>
+                    <p>Github Followers: {data.followers}</p>
+                    <img src={data.avatar_url} alt="GitHub Avatar" width={300} />
+                </>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+}
 
-// export default Github;
+export default Github;
